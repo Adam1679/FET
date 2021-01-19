3b36a892d6004bdfa675441e06d4174551a7fd89
adam <adam1679@163.com>
2021-01-18 23:49:39 -0500

    改了loss

--- models/feteldeep.py
+++ models/feteldeep.py
@@ -99,6 +99,8 @@ class BaseResModel(nn.Module):
     #     return loss
 
     def get_loss(self, true_type_vecs, scores, margin=1.0, person_loss_vec=None) :
+        import pdb;
+        pdb.set_trace ()
         return F.binary_cross_entropy (scores, true_type_vecs)
 
     def inference(self, scores, is_torch_tensor=True):
