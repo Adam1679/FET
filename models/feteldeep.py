f4194ed5252f51fd6857b083444a4aaf881e39fa
adam <adam1679@163.com>
2021-01-22 04:41:40 -0500

    requires_grad=(embedding=True)

--- models/feteldeep.py
+++ models/feteldeep.py
@@ -318,7 +318,7 @@ class NoName(BaseResModel):
         if type_emb_path is not None :
             print ("==> load pretrain type embedding")
             self.pre_train_type_embedding = torch.autograd.Variable (
-                torch.from_numpy (self._load_type_emb (type_emb_path, self.type_id_dict)), requires_grad=False)
+                torch.from_numpy (self._load_type_emb (type_emb_path, self.type_id_dict)), requires_grad=True)
             self.pre_train_type_embedding = self.pre_train_type_embedding.float ()
             self.pre_train_type_embedding = self.pre_train_type_embedding.to (device)
         else :
