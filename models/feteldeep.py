d64c92782aca1f2109ae8acda4ff10c8c479efe9
adam <adam1679@163.com>
2021-01-22 21:56:17 -0500

    不把entity vec当作filter，而是直接当做input

--- models/feteldeep.py
+++ models/feteldeep.py
@@ -346,7 +346,7 @@ class NoName(BaseResModel):
         if self.copy :
             self.alpha = nn.Sequential (nn.Linear (1, 1), nn.Sigmoid ())
         self.generate_mode = nn.Linear (hidden_size, self.n_types)
-        self.copy_mode = nn.Sequential (nn.Linear (self.n_types + 1, hidden_size),
+        self.copy_mode = nn.Sequential (nn.Linear (self.n_types + 1 + hidden_size, hidden_size),
                                         nn.ReLU (),
                                         nn.BatchNorm1d (hidden_size),
                                         nn.Dropout (dropout),
@@ -405,7 +405,7 @@ class NoName(BaseResModel):
         state = self.encoder (cat_output)  # (B, D)
         g = self.generate_mode (state)  # (B, type_dim)
         if self.copy :
-            c = self.copy_mode (torch.cat ((entity_vecs, el_probs.unsqueeze (1)), dim=1))  # (B, D)
+            c = self.copy_mode (torch.cat ((state, entity_vecs, el_probs.unsqueeze (1)), dim=1))  # (B, D)
             c = F.relu (c)
             logits = c + g
         else :
